dd0f00bf92aa3bc62ddb95450f1ccd2fa2478c9c
chaitali200219 <sulechaitali19@gmail.com>
2024-08-28 14:08:36 +0530

    question_changes

---
+++ quiz_app/question_management/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Questions, Option
+
+@admin.register(Questions)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('content', 'question_type', 'marks', 'created_at')
+
+@admin.register(Option)
+class OptionAdmin(admin.ModelAdmin):
+    list_display = ('question', 'content', 'is_correct', 'status')
+
---
+++ quiz_app/question_management/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+
+# Create your models here.
+
+
+
+class Questions(models.Model):
+    QUESTION_TYPE_CHOICES = [
+        ('MCQ', _('Multiple Choice')),
+        ('True', _('True')),
+        ('False', _('False')),
+    ]
+    # question_id=models.AutoField(primary_key=True)
+    # text of the question is content
+    content=models.CharField(max_length=255)
+    # type of the question rather mcq or t/f
+    question_type=models.CharField(max_length=50,choices=QUESTION_TYPE_CHOICES)
+    # marks allocated to each questions
+    marks = models.PositiveIntegerField()
+    # timestamp when the question was created
+    created_at=models.DateTimeField(default=timezone.now)
+    
+    
+    def __str__(self):
+        return self.content
+    
+    
+class Option(models.Model):
+    STATUS_CHOICES=[
+        ('active',_('Active')),
+        ('inactive',_('Inactive')),
+    ]    
+    # Option_id=models.AutoField(primary_key=True)
+    # key field linking with questions
+    question=models.ForeignKey(Questions,on_delete=models.CASCADE,related_name="options")
+    # text field
+    content = models.CharField(max_length=255)
+    # check whether the option is correct or not
+    is_correct = models.BooleanField(default=False)
+    # active or inactive
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='active')
+
+    def __str__(self):
+        return self.content
+    
+
+
+# class Tag(models.Model):
+#     tag_id=models.AutoField(primary_key=True)
+#     # each tag realted to one question
+#     question=models.ForeignKey(Questions,on_delete=models.CASCADE,related_name='tags')
+#     tag_name=models.CharField(max_length=100)
+#     # type of question which is assocaited with teh tag
+#     question_type=models.CharField(max_length=50)
+    
+    
+#     def __str__(self):
+#         return self.tag_name
