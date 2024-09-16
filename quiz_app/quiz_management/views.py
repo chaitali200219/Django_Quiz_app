eb65c8115f67705296dd1458da9eefadd9b26de2
maneabhay16 <maneabhay16@gmail.com>
2024-09-16 09:46:09 +0530

    save

--- quiz_app/quiz_management/views.py
+++ quiz_app/quiz_management/views.py
@@ -2,18 +2,6 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from .models import Quiz
-<<<<<<< HEAD
-from .serializers import QuizSerializer 
-from user_management.permissions import IsStudentOrTeacher, IsAdminOrTeacher
-from django.contrib.auth import authenticate
-from rest_framework.permissions import AllowAny, IsAuthenticated
-
-
-class QuizListView(generics.ListAPIView):
-    serializer_class = QuizSerializer
-    permission_classes = [IsAuthenticated]
-    
-=======
 from .serializers import QuizSerializer
 from .permissions import IsTeacher,IsStudent
 
@@ -22,7 +10,6 @@ class QuizListView(generics.ListAPIView):
     permission_classes = [IsStudent | IsTeacher]
 
 
->>>>>>> 0d6686cf2341ceb701c1b7bb99a1a021cfb328a7
     def get_queryset(self):
         """
         Returns all quizzes.
@@ -32,12 +19,7 @@ class QuizListView(generics.ListAPIView):
 class QuizCreateView(generics.CreateAPIView):
     
     serializer_class = QuizSerializer
-<<<<<<< HEAD
-    permission_classes=[IsAdminOrTeacher]
-    
-=======
     permission_classes=[IsTeacher]
->>>>>>> 0d6686cf2341ceb701c1b7bb99a1a021cfb328a7
 
 class QuizUpdateView(generics.UpdateAPIView):
     permission_classes=[IsAdminOrTeacher]
